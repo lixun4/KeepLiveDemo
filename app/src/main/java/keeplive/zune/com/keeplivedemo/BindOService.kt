06ac0b201adf7e8ef7cd20dcd36b67d25720787d
xuqianqian <951649157@qq.com>
2019-06-09 01:10:42 +0800

    test

--- app/src/main/java/keeplive/zune/com/keeplivedemo/BindOService.kt
+++ app/src/main/java/keeplive/zune/com/keeplivedemo/BindOService.kt
@@ -1,12 +1,17 @@
 package keeplive.zune.com.keeplivedemo
 
 import android.content.Intent
+import android.os.Handler
+import android.os.Message
 import android.support.v4.app.JobIntentService
 
 /**
  * Created by leigong2 on 2018-06-16 016.
  */
 class BindOService: JobIntentService() {
+    private var mHandler: Handler? = null
+    private var sendMsgThread: Thread? = null
+    private var running = true
     override fun onHandleWork(intent: Intent) {
 
     }
@@ -14,15 +19,37 @@ class BindOService: JobIntentService() {
     override fun onCreate() {
         super.onCreate()
         println("zune: 测试bindservice")
+        mHandler = Handler(Handler.Callback {
+           TxtLog.writeDataToSDCard("123455")
+            false
+        })
+
+        sendMsgThread = object : Thread() {
+            override fun run() {
+                while (running && mHandler != null) {
+                    mHandler!!.sendMessage(Message())
+
+                    try {
+                        Thread.sleep(1*60*1000)
+                    } catch (e: InterruptedException) {
+                        e.printStackTrace()
+                    }
+
+                }
+            }
+        }
+        (sendMsgThread as Thread).start()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        println("zune: 测试bindservice start")
+        TxtLog.writeDataToSDCard("zune: 测试bindservice start")
         return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        running=false
+        TxtLog.writeDataToSDCard("zune: 测试bindservice onDestroy")
         println("zune: 测试bindservice onDestroy")
     }
 }
